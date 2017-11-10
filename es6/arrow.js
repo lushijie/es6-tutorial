76e5498a45c56df096fa54a7fb859d167fcd9d73
lushijie <lushijie1218@126.com>
2017-11-10 18:44:48 +0800

    add

--- es6/arrow.js
+++ es6/arrow.js
@@ -2,14 +2,13 @@
 * @Author: lushijie
 * @Date:   2017-11-07 15:12:48
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-11-10 18:29:26
+* @Last Modified time: 2017-11-10 18:44:07
 */
 
 // 箭头函数使用注意点
-// （1）函数体内的this对象，就是定义时所在的对象，而不是使用时所在的对象。
-// （2）不可以当作构造函数，也就是说，不可以使用new命令，否则会抛出一个错误。
-// （3）不可以使用arguments对象，该对象在函数体内不存在。如果要用，可以用 rest 参数代替。
-// （4）不可以使用yield命令，因此箭头函数不能用作 Generator 函数。
+// （1）函数体内的this对象，就是定义时所在的对象，而不是使用时所在的对象
+// （2）不可以当作构造函数，也就是说，不可以使用new命令，否则会抛出一个错误
+// （3）不可以使用yield命令，因此箭头函数不能用作 Generator 函数
 
 // (function (a) {
 //   console.log(a);
@@ -25,9 +24,10 @@
 // let f = function () { return 5 };
 
 
-// // this对象的指向是可变的，但是在箭头函数中，它是固定的。
+// // this对象的指向是可变的，但是在箭头函数中，它是固定的
 // // 箭头函数中的this总是指向函数【定义生效时】所在的对象
-// // 箭头函数根本没有自己的this，导致内部的this就是外层代码块的this。正是因为它没有this，所以也就不能用作构造函数。
+// // 箭头函数根本没有自己的this，内部的this就是外层代码块的this因为它没有this，也就不能用作构造函数
+
 // function Timer() {
 //   this.s1 = 0;
 //   this.s2 = 0;
@@ -66,7 +66,7 @@
 // func.init(); // global
 
 
-
+// // 多层级的箭头函数
 // function foo() {
 //   return () => {
 //     return () => {
@@ -78,17 +78,24 @@
 // }
 
 // let f = foo.call({id: 1});
-
 // let t1 = f.call({id: 2})()(); // id: 1
 // let t2 = f().call({id: 3})(); // id: 1
 // let t3 = f()().call({id: 4}); // id: 1
 
 
-// 箭头函数内部的变量arguments，其实是函数foo的arguments变量。
-function foo() {
-  setTimeout((a) => {
-    console.log('args:', arguments);
-  }, 100);
-}
+// // 箭头函数内部的变量arguments，其实是函数foo的arguments变量
+// // 如果要用，可以用 rest 参数代替
+// function foo() {
+//   setTimeout((a) => {
+//     console.log('args:', arguments);
+//   }, 100);
+// }
+// foo(2, 4, 6, 8)
 
-foo(2, 4, 6, 8)
+
+// // 由于箭头函数没有自己的this，所以当然也就不能用call()、apply()、bind()这些方法去改变this的指向
+// (function() {
+//   return [
+//     (() => this.x).bind({ x: 'inner' })(); // outer
+//   ];
+// }).call({ x: 'outer' });
