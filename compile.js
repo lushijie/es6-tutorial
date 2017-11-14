eb5102e3d7ea5b0dc78c20c71c6bbb5530268508
lushijie <lushijie1218@126.com>
2017-11-14 12:25:51 +0800

    add

--- compile.js
+++
@@ -1,9 +0,0 @@
-/*
-* @Author: lushijie
-* @Date:   2017-11-14 10:14:38
-* @Last Modified by:   lushijie
-* @Last Modified time: 2017-11-14 10:15:58
-*/
-babel es6/ --out-dir build/es6
-babel object/ --out-dir build/object
-babel node/ --out-dir build/node
---
+++ compile.sh
@@ -0,0 +1,3 @@
+babel es6/ --out-dir build/es6
+babel object/ --out-dir build/object
+babel node/ --out-dir build/node
