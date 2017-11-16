2fa2d4fc417c413cb6ec4255a43515cf9397f0d6
lushijie <lushijie1218@126.com>
2017-11-16 14:13:54 +0800

    add

--- es6/module.js
+++ es6/module.js
@@ -2,5 +2,11 @@
 * @Author: lushijie
 * @Date:   2017-11-10 10:01:40
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-11-16 14:03:49
+* @Last Modified time: 2017-11-16 14:06:14
 */
+
+// CommonJS模块
+// let { stat, exists, readFile } = require('fs');
+
+// ES6模块
+import { stat, exists, readFile } from 'fs';
