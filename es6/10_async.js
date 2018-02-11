95d45465d77f0528f974caaa5cc1e491b83e66f8
lushijie <lushijie1218@126.com>
2018-02-11 15:13:25 +0800

    async

--- es6/10_async.js
+++ es6/10_async.js
@@ -2,33 +2,31 @@
 * @Author: lushijie
 * @Date:   2017-11-06 18:03:07
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-11-07 10:53:12
+* @Last Modified time: 2018-02-11 15:12:52
 */
 
 // // 一、async 函数执行结果是一个 Promise
-// async function func() {
-//   console.log('This is in func');
+// async function func1() {
+//   // console.log('This is in func1');
 // }
-// console.log(typeof func); // function
-// func();
-// console.log(func()); // Promise
-// func().then(() => {
-//   console.log('resolve ok');
+// console.log('1', typeof func1); // function
+// console.log('1,', func1()); // Promise
+// func1().then(() => {
 // }).catch(e => {
 //   console.log(e);
 // });
 
 // // 二、async 返回值,作为 resolve 的值
-// async function func() {
+// async function func2() {
 //   return 123123;
 // }
-// func().then((data) => {
-//   console.log('get return data here', data); // 123123
+// func2().then((data) => {
+//   console.log('2,', data); // 123123
 // });
 
 
 // // 三、普通函数返回 Promise
-// function sleep(time) {
+// function sleep3(time) {
 //   return new Promise((resolve, reject) => {
 //     setTimeout(() => {
 //       resolve();
@@ -37,18 +35,17 @@
 // }
 
 // (async function() {
-//   console.log(new Date());
-//   await(sleep(3000));
-//   console.log('await1')
-//   sleep(3000).then(() => {
-//     console.log('await2');
+//   console.log('3', new Date()); // 3秒前
+//   await(sleep3(3000));
+//   sleep3(3000).then(() => {
+//     console.log('3', 'await2');
 //   });
-//   console.log(new Date());
+//   console.log('3', new Date()); // 3秒后
 // })();
 
 
 // // 四、非依赖的两个函数可以并行执行 1
-// function sleep(time) {
+// function sleep4(time) {
 //   return new Promise((resolve, reject) => {
 //     setTimeout(() => {
 //       resolve(time);
@@ -57,56 +54,54 @@
 // }
 
 // (async function() {
-//   console.log(new Date())
-//   let [a, b] = await Promise.all([sleep(3000), sleep(5000)]);
-//   console.log(a, b);
-//   console.log(new Date())
+//   console.log('4', new Date()); // 5秒前
+//   let [a, b] = await Promise.all([sleep4(3000), sleep4(5000)]);
+//   console.log('4,', a, b); // 3000 5000
+//   console.log('4', new Date()); // 5秒后
 // })();
 
 
-// //五、非依赖的两个函数可以并行执行 2
-// function sleep(time) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(time);
-//     }, time)
-//   });
-// }
+//五、非依赖的两个函数可以并行执行 2
+function sleep5(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(time);
+    }, time)
+  });
+}
 
-// (async function() {
-//   console.log(new Date());
-//   let a = sleep(5000);
-//   let b = sleep(2000);
-//   let aout = await a; // 一旦遇到await就会先返回，等到异步操作完成，再接着执行函数体内后面的语句
-//   console.log('a', aout);
-//   let bout = await b;
-//   console.log('b', bout);
-//   console.log('---');
-//   console.log(new Date());
-// })();
+(async function() {
+  console.log('5,', new Date());
+  let a = sleep5(5000);
+  let b = sleep5(2000);
+  let aout = await a; // 一旦遇到await就会先返回，等到异步操作完成，再接着执行函数体内后面的语句
+  console.log('5a,', aout);
+  let bout = await b;
+  console.log('5b,', bout);
+  console.log('5,', new Date());
+})();
 
 
-// // 六、async catch
-// function func() {
-//   console.log(123);
-// }
-// (async function() {
-//   await func();
-// })().then(() => {
-//   console.log(456);
-// }).catch(err => {
-//   console.log(err);
-// });
+// 六、async catch
+function func6() {
+}
+(async function() {
+  await func6();
+})().then(() => {
+  console.log('6,', 456);
+}).catch(err => {
+  console.log(err);
+});
 
-// // 七、async return
-// async function f() {
-//   // return Promise.reject(123);
-//   throw new Error('出错了'); // catch
-// }
+// 七、async return
+async function func7() {
+  return Promise.reject(123);
+  // throw new Error('出错了'); // catch
+}
 
-// f().then(
-//   v => console.log(v),
-//   e => console.log(e, 'reject') // 123, 'reject'
-// ).catch(e => {
-//   console.log(e);
-// });
+func7().then(
+  v => console.log('7,', v), // resolve
+  e => console.log('7,', e, 'reject') // 'reject'
+).catch(e => {
+  console.log(e);
+});
